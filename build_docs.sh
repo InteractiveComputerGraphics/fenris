d2001491bd8b984254eb9b8e5288e329db711d13
Andreas Longva <andreas.b.longva@gmail.com>
2021-06-16 13:06:15 +0200

    Avoid building docs twice in build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -1,2 +1,12 @@
-cargo doc
-RUSTDOCFLAGS="--html-in-header assets/doc-header.html" cargo rustdoc --open
+#!/bin/bash
+
+# This script injects a header that lets us write our Rust documentation with TeX formulas rendered by KaTeX.
+# This approach only works when providing `cargo doc` with an **absolute path** to the header file, hence
+# we obtain the path relative to this script file and build the docs with this header injection.
+
+# From: https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+HEADER_PATH="$SCRIPT_DIR/assets/doc-header.html"
+
+# Note: command line arguments are forwarded to the final invocation
+RUSTDOCFLAGS="--html-in-header $HEADER_PATH" cargo doc $@
