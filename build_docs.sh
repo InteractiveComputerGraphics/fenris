f70228135c2860f1ee900c15bd47c176a5ddcc59
Andreas Longva <andreas.b.longva@gmail.com>
2022-10-25 11:25:48 +0200

    Update KaTeX and fix rendering issues

--- build_docs.sh
+++ build_docs.sh
@@ -9,5 +9,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 HEADER_PATH="$SCRIPT_DIR/assets/doc-header.html"
 BEFORE_CONTENT_PATH="$SCRIPT_DIR/assets/doc-before-content.html"
 
+# Copy assets to other crates in the workspace, so that
+# they can use the same assets for docs.rs
+cp $HEADER_PATH "$SCRIPT_DIR/fenris-solid/assets/doc-header.html"
+cp $BEFORE_CONTENT_PATH "$SCRIPT_DIR/fenris-solid/assets/doc-before-content.html"
+
 # Note: command line arguments are forwarded to the final invocation
 RUSTDOCFLAGS="--html-in-header $HEADER_PATH --html-before-content $BEFORE_CONTENT_PATH" cargo doc $@
