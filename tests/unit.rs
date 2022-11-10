19988be4b6fb56fc580051dd0f378f102c46f13c
Fabian Loeschner <loeschner@cs.rwth-aachen.de>
2022-11-10 16:02:27 +0100

    Formatting

--- tests/unit.rs
+++ tests/unit.rs
@@ -1,16 +1,16 @@
-use std::path::Path;
-use nalgebra::{DefaultAllocator, DimName};
-use nalgebra::allocator::Allocator;
 use fenris::io::vtk::{FiniteElementMeshDataSetBuilder, VtkCellConnectivity};
 use fenris::mesh::Mesh;
+use nalgebra::allocator::Allocator;
+use nalgebra::{DefaultAllocator, DimName};
+use std::path::Path;
 
 mod unit_tests;
 
 fn export_mesh_vtk<D, C>(test_name: &str, file_stem: &str, mesh: &Mesh<f64, D, C>)
-    where
-        D: DimName,
-        DefaultAllocator: Allocator<f64, D>,
-        C: VtkCellConnectivity,
+where
+    D: DimName,
+    DefaultAllocator: Allocator<f64, D>,
+    C: VtkCellConnectivity,
 {
     let output_path = Path::new("data/unit_tests/")
         .join(test_name)
@@ -19,4 +19,3 @@ fn export_mesh_vtk<D, C>(test_name: &str, file_stem: &str, mesh: &Mesh<f64, D, C
         .try_export(output_path)
         .expect("Export failure is a test failure")
 }
-
