8e6abfb14a8856228f949ed3c002ba4f00e9fd86
Andreas Longva <andreas.b.longva@gmail.com>
2023-02-20 10:30:30 +0100

    Formatting for benchmarks

--- benches/assembly.rs
+++ benches/assembly.rs
@@ -1,28 +1,29 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::hint::black_box;
-use nalgebra::{DefaultAllocator, DVector, DVectorSlice};
-use nalgebra_sparse::CsrMatrix;
-use nalgebra_sparse::pattern::SparsityPattern;
 use fenris::assembly::global::CsrAssembler;
 use fenris::assembly::local::{ElementEllipticAssemblerBuilder, QuadratureTable};
 use fenris::assembly::operators::LaplaceOperator;
 use fenris::element::ElementConnectivity;
-use fenris::mesh::Mesh;
 use fenris::mesh::procedural::create_unit_box_uniform_tet_mesh_3d;
+use fenris::mesh::Mesh;
 use fenris::quadrature::CanonicalStiffnessQuadrature;
 use fenris::SmallDim;
-use fenris_traits::allocators::{DimAllocator};
+use fenris_traits::allocators::DimAllocator;
+use nalgebra::{DVector, DVectorSlice, DefaultAllocator};
+use nalgebra_sparse::pattern::SparsityPattern;
+use nalgebra_sparse::CsrMatrix;
+use std::hint::black_box;
 
-fn assemble_poisson_into_serial<D, C>(matrix: &mut CsrMatrix<f64>,
-                                      assembler: &CsrAssembler<f64>,
-                                      u: DVectorSlice<f64>,
-                                      qtable: &impl QuadratureTable<f64, D, Data=()>,
-                                      mesh: &Mesh<f64, D, C>)
--> eyre::Result<()>
+fn assemble_poisson_into_serial<D, C>(
+    matrix: &mut CsrMatrix<f64>,
+    assembler: &CsrAssembler<f64>,
+    u: DVectorSlice<f64>,
+    qtable: &impl QuadratureTable<f64, D, Data = ()>,
+    mesh: &Mesh<f64, D, C>,
+) -> eyre::Result<()>
 where
     D: SmallDim,
-    C: ElementConnectivity<f64, GeometryDim=D, ReferenceDim=D>,
-    DefaultAllocator: DimAllocator<f64, D>
+    C: ElementConnectivity<f64, GeometryDim = D, ReferenceDim = D>,
+    DefaultAllocator: DimAllocator<f64, D>,
 {
     let element_assembler = ElementEllipticAssemblerBuilder::new()
         .with_u(u)
@@ -34,15 +35,15 @@ where
 }
 
 fn assemble_poisson_pattern_serial<D, C>(
-                                      assembler: &CsrAssembler<f64>,
-                                      u: DVectorSlice<f64>,
-                                      qtable: &impl QuadratureTable<f64, D, Data=()>,
-                                      mesh: &Mesh<f64, D, C>)
-                                      -> SparsityPattern
-    where
-        D: SmallDim,
-        C: ElementConnectivity<f64, GeometryDim=D, ReferenceDim=D>,
-        DefaultAllocator: DimAllocator<f64, D>
+    assembler: &CsrAssembler<f64>,
+    u: DVectorSlice<f64>,
+    qtable: &impl QuadratureTable<f64, D, Data = ()>,
+    mesh: &Mesh<f64, D, C>,
+) -> SparsityPattern
+where
+    D: SmallDim,
+    C: ElementConnectivity<f64, GeometryDim = D, ReferenceDim = D>,
+    DefaultAllocator: DimAllocator<f64, D>,
 {
     let element_assembler = ElementEllipticAssemblerBuilder::new()
         .with_u(u)
@@ -63,8 +64,14 @@ pub fn poisson_assembly_serial(c: &mut Criterion) {
         let mut matrix = CsrMatrix::try_from_pattern_and_values(pattern, vec![0.0; nnz]).unwrap();
         let u = DVector::repeat(matrix.nrows(), 0.0);
         let qtable = tet4_mesh.canonical_stiffness_quadrature();
-        c.bench_function(&format!("serial assembly poisson stiffness matrix tet4 (res={res})"),
-                         |b| b.iter(|| assemble_poisson_into_serial(&mut matrix, &assembler, DVectorSlice::from(&u), &qtable, &tet4_mesh)));
+        c.bench_function(
+            &format!("serial assembly poisson stiffness matrix tet4 (res={res})"),
+            |b| {
+                b.iter(|| {
+                    assemble_poisson_into_serial(&mut matrix, &assembler, DVectorSlice::from(&u), &qtable, &tet4_mesh)
+                })
+            },
+        );
     }
 }
 
@@ -75,10 +82,25 @@ pub fn poisson_pattern_assembly_serial(c: &mut Criterion) {
         let tet4_mesh = create_unit_box_uniform_tet_mesh_3d(res);
         let u = DVector::repeat(tet4_mesh.vertices().len(), 0.0);
         let qtable = tet4_mesh.canonical_stiffness_quadrature();
-        c.bench_function(&format!("serial pattern assembly poisson stiffness matrix tet4 (res={res})"),
-                         |b| b.iter(|| black_box(assemble_poisson_pattern_serial(&assembler, DVectorSlice::from(&u), &qtable, &tet4_mesh))));
+        c.bench_function(
+            &format!("serial pattern assembly poisson stiffness matrix tet4 (res={res})"),
+            |b| {
+                b.iter(|| {
+                    black_box(assemble_poisson_pattern_serial(
+                        &assembler,
+                        DVectorSlice::from(&u),
+                        &qtable,
+                        &tet4_mesh,
+                    ))
+                })
+            },
+        );
     }
 }
 
-criterion_group!(serial_assembly, poisson_assembly_serial, poisson_pattern_assembly_serial);
+criterion_group!(
+    serial_assembly,
+    poisson_assembly_serial,
+    poisson_pattern_assembly_serial
+);
 criterion_main!(serial_assembly);
